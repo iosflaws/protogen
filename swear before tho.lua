1d42ca58f78586c3246dffc86c74dad6d78dc295
flaws <110755184+iosflaws@users.noreply.github.com>
2022-09-20 14:43:22 -0600

    Update swear before tho.lua

--- swear before tho.lua
+++ swear before tho.lua
@@ -1,5 +1,10 @@
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+	Title = "Notification";
+	Text = "Executed! Thanks for using FlawedWare! This is a Anti-ban.";
+	Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 16;
 --sacred isnt here
-
+--flaws1337 reuploaded this.
 local logs = true --Enables logs or not
 local ChatMain = require(game:GetService("Players").LocalPlayer.PlayerScripts.ChatScript.ChatMain)
 local blacklistedFirstCharacter = {"a","b","c","d","e","f","g","h","i","j","k","m","n","l","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","M","N","L","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0","/","@","?",}
